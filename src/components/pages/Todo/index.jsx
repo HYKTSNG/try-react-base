474b38aec950af132cb125b294d4abd40676bbed
Shin <Sin5hayaK@gmail.com>
2020-07-25 16:56:56 +0900

    Todo App  : Formの作成、

--- src/components/pages/Todo/index.jsx
+++ src/components/pages/Todo/index.jsx
@@ -1,11 +1,72 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core"
+import { useState } from "react"
 
 /**
  * TODOアプリ
  */
+
 export const Todo = () => {
-  return <div css={root}>Todo App</div>
+  // 初期値のList
+  const initialState = [
+    {
+      isCompleted: false,
+      task: "sososo",
+    },
+    {
+      isCompleted: false,
+      task: "Learn vue.js",
+    },
+  ]
+  const [text, setTask] = useState("")
+  const [todos, setTodo] = useState(initialState)
+
+  const handleNewTask = (e) => {
+    setTask(e.target.value)
+  }
+
+  const onClickAdd = (e) => {
+    console.log(e)
+  }
+
+  return (
+    <div>
+      <div css={root}>Todo App</div>
+      <div css={inputWrapper}>
+        <input
+          // FORM の仕様
+          onChange={handleNewTask}
+          placeholder="Add New Task"
+          value={text}
+        />
+        <button
+          // TODO リストに追加ボタンを実装
+          onClick={(e) => {
+            onClickAdd(e)
+          }}
+        >
+          追加
+        </button>
+      </div>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo.task}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
-const root = css``
+const root = css`
+  font-size: 56px;
+  padding-top: 56px;
+  padding-left: 64px;
+`
+const inputWrapper = css`
+  font-size: 24px;
+  padding-top: 24px;
+  padding-left: 56px;
+`
+// 最後に色付ける
+// background-color: aquamarine;
